e6bc001719cb11d64f3003be308067a4e1424799
def-SpaceWar <aryanahire9086360780@gmail.com>
2021-08-25 16:55:15 -0400

    HOPEFULLY ITS GOOD NOW

--- lua/lualine/themes/monokai_apprentice.lua
+++ lua/lualine/themes/monokai_apprentice.lua
@@ -14,28 +14,28 @@ local colors = {
 return {
   normal = {
     a = {bg = colors.red, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.darkgray, fg = colors.white},
+    b = {bg = colors.magenta, fg = colors.white},
     c = {bg = colors.darkgray, fg = colors.white}
   },
   insert = {
     a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.darkgray, fg = colors.white},
+    b = {bg = colors.magenta, fg = colors.white},
     c = {bg = colors.darkgray, fg = colors.white}
   },
   visual = {
     a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.white, fg = colors.black},
-    c = {bg = colors.white, fg = colors.black}
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
   },
   replace = {
     a = {bg = colors.green, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.darkgray, fg = colors.white},
-    c = {bg = colors.white, fg = colors.black}
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
   },
   command = {
     a = {bg = colors.white, fg = colors.black, gui = 'bold'},
-    b = {bg = colors.darkgray, fg = colors.white},
-    c = {bg = colors.white, fg = colors.black}
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
   },
   inactive = {
     a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
