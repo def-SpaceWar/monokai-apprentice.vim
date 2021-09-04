45da20cdfa0e750fde6cd32cc6154b4d1db7cc1a
def-SpaceWar <aryanahire9086360780@gmail.com>
2021-09-04 15:02:00 -0400

    Added a variant of monokai's apprentice!

---
+++ lua/lualine/themes/monokai_apprentice_old.lua
@@ -0,0 +1,45 @@
+local colors = {
+  black        = '#151619',
+  white        = '#C0C3CF',
+  red          = '#F63954',
+  green        = '#6FBB6A',
+  blue         = '#30B6D7',
+  yellow       = '#EBCC61',
+  gray         = '#767676',
+  darkgray     = '#3D3E3F',
+  magenta      = '#F97933',
+  inactivegray = '#C0C3CF',
+}
+-- LuaFormatter on
+return {
+  normal = {
+    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
+  },
+  insert = {
+    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
+  },
+  visual = {
+    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
+  },
+  replace = {
+    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
+  },
+  command = {
+    a = {bg = colors.white, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.magenta, fg = colors.black},
+    c = {bg = colors.darkgray, fg = colors.white}
+  },
+  inactive = {
+    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
+    b = {bg = colors.darkgray, fg = colors.gray},
+    c = {bg = colors.darkgray, fg = colors.gray}
+  }
+}
